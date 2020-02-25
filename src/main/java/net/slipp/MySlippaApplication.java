e3f76d54562b53f468bcc7cba4a808f9fbce25be
지경호 <smilejkh90@gmail.com>
2020-02-25 16:07:26 +0900

    perfect commit

---
+++ src/main/java/net/slipp/MySlippaApplication.java
@@ -0,0 +1,13 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlippaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlippaApplication.class, args);
+	}
+
+}
